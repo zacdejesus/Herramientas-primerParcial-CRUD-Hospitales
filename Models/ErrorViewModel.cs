83206d1c77c69f183f0da5cfcdab4475e0399c03
Alejandro De Jesus <zadkielsi@outlook.com>
2023-04-22 12:53:35 -0300

    readme updated

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace parcial1_hospitales.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
